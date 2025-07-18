d5a1544d88963d607bc85826437c4a317cf56834
John Graham-Cumming <github@jgc.org>
2025-07-18 12:41:20 +0100

    My selection of watch faces

--- movement_config.h
+++ movement_config.h
@@ -37,6 +37,9 @@ const watch_face_t watch_faces[] = {
     alarm_face,
     temperature_display_face,
     voltage_face,
+    accelerometer_status_face,
+    light_sensor_face,
+    tally_face,
     settings_face,
     set_time_face
 };
@@ -49,7 +52,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 4)
+#define MOVEMENT_SECONDARY_FACE_INDEX 0
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
